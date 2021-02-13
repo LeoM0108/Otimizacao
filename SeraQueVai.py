0d5758608f788680f7b14c2da1037e2bc44f0b84
LeoM0108 <69467961+LeoM0108@users.noreply.github.com>
2021-02-13 13:33:24 -0300

    Create SeraQueVai.py

---
+++ SeraQueVai.py
@@ -0,0 +1,10 @@
+class Card:
+    def __init__(self, nome, idade, classe):
+        self.nome = nome
+        self.idade = idade      
+        self.classe = classe
+
+    
+Card1 = ("Leo", 21, "B+")
+
+print(Card1)
